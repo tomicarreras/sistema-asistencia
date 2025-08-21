f7a3016790966ecbc6316a3f3ab3bcee2ef3d047
tomicarreras <tomicarreras97@gmail.com>
2025-08-20 23:16:42 -0300

    Arreglado problema log in

--- components/dashboard/navbar.tsx
+++ components/dashboard/navbar.tsx
@@ -27,7 +27,7 @@ export default function Navbar() {
     <nav className="bg-white shadow-sm border-b">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
-          <div className="flex items-center">
+          <div className="flex items-left">
             <img src="/logo.png" alt="Logo del Sistema" className="h-10 mx-auto mb-2" />
           </div>
 
